a6b1fe647f8e19c37b913963fd3f60dc06c1a9a9
Austin Sun <sunaustin8@gmail.com>
2019-04-08 01:40:23 -0500

    player summary fix

--- AndroidStudioProjects/cs498teamassign5/app/src/main/java/com/example/cs498teamassign5/PlayerStat.java
+++ AndroidStudioProjects/cs498teamassign5/app/src/main/java/com/example/cs498teamassign5/PlayerStat.java
@@ -1,29 +1,36 @@
 package com.example.cs498teamassign5;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class PlayerStat extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class PlayerStat extends Activity implements View.OnClickListener {
+    private ArrayList<GameInfo> allGames;
+    private static final int MAIN_ACTIVITY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_stat);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        allGames = new ArrayList<>();
+
     }
 
+    public void onClick(View v){
+        if(v.getId() == R.id.NewGameButton) {
+            Intent intent = new Intent(this, MainActivity.class);
+            GameInfo gameInfo = new GameInfo();
+            intent.putExtra("gameInfo", gameInfo);
+            startActivityForResult(intent, MAIN_ACTIVITY_REQUEST);
+        }
+    }
 }
